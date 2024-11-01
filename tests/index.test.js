22fdbdb501899521d7eed849fc169f66068c6245
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-11-01 11:57:46 -0700

    change line 9 to validate 200 OK status on pageload, confirm successful test.

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,5 +6,5 @@ test('validate the page responds with 200 OK', async ({ page }) => {
    const response = await page.goto('https://news.ycombinator.com/newest');
   
     // Expect the title to contain 'ycombinator'
-    expect(response.status()).toBe(404);
+    expect(response.status()).toBe(200);
   });
