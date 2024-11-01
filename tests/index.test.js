97ad28157f2a81a0a483a497112074667e459c3f
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-11-01 11:58:53 -0700

    adjust formatting of the script.

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,10 +1,10 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
 // tests //
 
-test('validate the page responds with 200 OK', async ({ page }) => {
-   const response = await page.goto('https://news.ycombinator.com/newest');
-  
-    // Expect the title to contain 'ycombinator'
-    expect(response.status()).toBe(200);
-  });
+test("validate the page responds with 200 OK", async ({ page }) => {
+  const response = await page.goto("https://news.ycombinator.com/newest");
+
+  // Expect the title to contain 'ycombinator'
+  expect(response.status()).toBe(200);
+});
