dac82841f0feb6607d7896673dcfda1756884256
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-11-02 14:43:08 -0700

    console.log() the body of the page upon load to visualize elements of the page.

--- tests/index.test.js
+++ tests/index.test.js
@@ -22,3 +22,12 @@ test("validate pagename contains '/newest' in the url", async ({ page }) => {
   // Expect the title to contain 'ycombinator'
   expect(response_url).toContain('/newest');
 });
+
+test ("validate upon pageload, the first 100 articles displayed are sorted in order from newest to oldest", async ({ page }) => {
+  await page.goto("https://news.ycombinator.com/newest");
+  
+  const articles = await page.evaluate(() => document.body.innerHTML);
+  console.log("Body HTML content:", articles);
+
+
+});
