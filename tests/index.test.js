ce8aa4d7e0b50272e241cbf864071aea7f1e996a
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-11-02 10:32:33 -0700

    add tests to validate the presence of 'ycombinator' and '/newest' in the url of the page. Tested initially for false positives, ensured tests pass with accurate expected response.

--- tests/index.test.js
+++ tests/index.test.js
@@ -5,6 +5,20 @@ const { test, expect } = require("@playwright/test");
 test("validate the page responds with 200 OK", async ({ page }) => {
   const response = await page.goto("https://news.ycombinator.com/newest");
 
-  // Expect the title to contain 'ycombinator'
+  // Expect the page to load with a 200 OK status
   expect(response.status()).toBe(200);
+});
+
+test("validate pagename contains 'ycombinator' in the url", async ({ page }) => {
+  const response = await page.goto("https://news.ycombinator.com/newest");
+  const response_url = (response._initializer.url);
+  // Expect the title to contain 'ycombinator'
+  expect(response_url).toContain('ycombinator');
+});
+
+test("validate pagename contains '/newest' in the url", async ({ page }) => {
+  const response = await page.goto("https://news.ycombinator.com/newest");
+  const response_url = (response._initializer.url);
+  // Expect the title to contain 'ycombinator'
+  expect(response_url).toContain('/old');
 });
