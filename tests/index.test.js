e1980165537e43f30ab0f33ee16c3916f9de917c
Antonio Cruz <antonio.cruz5612@gmail.com>
2024-11-08 16:01:28 -0800

    create a while loop that will locate and extract timestamp data, iterative against a targetArticleCount value of 100. Loop will populate an array of dateTimes, and prompt a search and button click to load more articles on the page until the condition of the loop is satisfied. Create an isSorted() function that can be called in the expect line of the test, which will map the dateTimes[] string array to numerical based dates array. This function will will loop over the dates array and return true or false if the subsequent indices of the array are in descending or ascending order. Utilize isSorted() in our expect line, and check for false positives by testing for .toBe(false).

--- tests/index.test.js
+++ tests/index.test.js
@@ -28,24 +28,66 @@ test("validate pagename contains '/newest' in the url", async ({ page }) => {
 test("validate upon pageload, the first 100 articles displayed are sorted in order from newest to oldest", async ({
   page,
 }) => {
+  // Visit the /newest path of the Hacker News forum
   await page.goto("https://news.ycombinator.com/newest");
-  // Locate all date elements inside article tags
-  const dateElements = await page.locator('span.age[title]');
-  // Get the count of date elements
-  const count = await dateElements.count();
-  // console.log("Tally of dateElements: ", count);
-  
   // Extract and parse all dates
-  let dates = [];
-  for (let i = 0; i < count; i++) {
-    const datetime = await dateElements.nth(i).getAttribute('title');
-    // Remove the numbers after the space from the dateString
-    const dateString = datetime.split(' ')[0];
+  let dateTimes = [];
+  // Establish a target for how many articles to parse datetime from
+  const targetArticleCount = 100;
+  
+  while (dateTimes.length < targetArticleCount) {
+    // Locate all date elements inside article tags
+    const dateElements = await page.locator('span.age[title]');
+    // Get the count of date elements
+    const count = await dateElements.count();
+    // console.log("Tally of dateElements: ", count);
 
-    // Parse the cleaned up date string
-    const date = new Date(dateString);
-    dates.push(date);
-    console.log(`Article ${i + 1} parsed date: ${date}`);
-    console.log("Dates Array: ", dates);
+    for (let i = 0; i < count; i++) {
+      if (dateTimes.length >= targetArticleCount) break;
+      // Grab the matching nth locator of i, return value within 'title'attribute. Store value as datetime.
+      const datetime = await dateElements.nth(i).getAttribute('title');
+      console.log("datetime variable: ",datetime)
+
+      if (datetime) {
+        // Remove the numbers after the space from the dateString
+        const dateString = datetime.split(' ')[0];
+        // Store dateString into dateTimes array
+        dateTimes.push(dateString);
+      }
+    }
+
+    if (dateTimes.length >= targetArticleCount) break;
+
+    // Find and click the "More" button to load more articles
+    const moreButton = await page.locator('a.morelink');
+    if (await moreButton.isVisible()) {
+      await moreButton.click();
+      await page.waitForLoadState('networkidle');
+    } else {
+      console.log("No more articles to load.");
+      break;
+    }
   }
+
+  // Log the collected datetimes
+  // console.log("Collected dateTimes: ", dateTimes);
+  // console.log("Total timestamps collected: ", dateTimes.length);
+
+  function isSorted(dateStrings) {
+
+    const dates = dateStrings.map(dateString => new Date(dateString));
+    
+    for (let i = 1; i < dates.length - 1; i++) {
+      if (dates[i] < dates[i + 1]) {
+        console.log(`Sorting error at index ${i}: ${dates[i]} is older than ${dates[i + 1]}`)
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // const sortingCheck = isSorted(dateTimes);
+  // console.log("Descending Order Sorting status: ", isSorted(dateTimes));
+  // console.log("Dates array: ", dateTimes);
+  expect(isSorted(dateTimes)).toBe(true);
 });
